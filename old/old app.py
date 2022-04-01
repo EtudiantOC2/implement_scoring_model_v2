9a689df701e53d5f5814dff9efa47b45067f050b
cpaulinec <Pauline_CASTORIADIS@connect-tech.sncf>
2022-04-01 13:51:41 +0200

    Version 9.6

---
+++ old/old app.py
@@ -0,0 +1,52 @@
+# Import des packages nécessaires
+import pandas as pd
+import pickle
+from flask import Flask, request, jsonify,render_template
+
+# Création de l'app
+app = Flask(__name__)
+
+# Petit test Hello World
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+# Sauvegarde du modèle
+model = pickle.load(open('C:\\Users\\pauline_castoriadis\\Documents\\implement_scoring_model_v2\\model\\model.pkl','rb'))
+
+# Chargement data
+def loading_csv_data(path):
+    '''
+    Retourne le dataframe à partir du chemin qu'on lui désigne
+    '''
+    df = pd.read_csv(path)
+    df.drop("Unnamed: 0", axis = 1, inplace = True)
+    return df
+
+# chargement des donnees
+data = loading_csv_data(r"C:\Users\pauline_castoriadis\Documents\implement_scoring_model_v2\data\df_test.csv")
+
+# Chargement prédiction
+def loading_predicted_data(df,id_col):
+    '''
+    Réalise la prédiction pour l'ensemble des clients contenus dans la base donnée
+    '''
+    selection_col =  list(df.loc[:, (df.columns != id_col)])
+    data_selection = df[selection_col].values
+    prediction =  pd.DataFrame(data = model.predict_proba(data_selection)[:, 0],columns=['prediction'])
+    result = pd.concat([df, prediction], axis = 1)
+    return result
+
+predicted_data = loading_predicted_data(data,'applicant_loan_id')
+
+# calcul du score et envoi d'un dico avec les infos id clients et prédiction
+@app.route('/api/<int:applicant_loan_id>')
+def mon_api(applicant_loan_id):
+    id_data = predicted_data[predicted_data['applicant_loan_id'] == int(applicant_loan_id)] # Données
+    output = round(id_data['prediction'].iat[0,],3) # Prédiction
+    dico = {'id': applicant_loan_id,
+            'prediction':output}
+    return jsonify(dico)
+
+if __name__ == "__main__":
+    app.run(debug = True)
